d1af1fc66efc184538c573b93d677ebc50c71c60
PawelRa <pawelrat@poczta.onet.pl>
2023-11-14 19:59:18 +0100

    EntityFrameworkCore

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,13 +1,7 @@
 ﻿using MotoApp.Repositores;
 using MotoApp.Entities;
 
-var employeeRepository = new EmployeeRepository();
-employeeRepository.Add(new Employee { FirstName = "Adam" });
-employeeRepository.Add(new Employee { FirstName = "Piotr" });
-employeeRepository.Add(new Employee { FirstName = "Zuzia" });
-employeeRepository.Save();
-
-var employeeGenericRepository = new GenericRepository<Employee, Guid>();
+var employeeGenericRepository = new GenericRepository<Employee>();
 employeeGenericRepository.Add(new Employee { FirstName = "Adam" });
 employeeGenericRepository.Add(new Employee { FirstName = "Zuzia" });
 employeeGenericRepository.Save();
--- MotoApp/Repositores/GenericRepository.cs
+++ MotoApp/Repositores/GenericRepository.cs
@@ -2,29 +2,25 @@
 
 namespace MotoApp.Repositores
 {
-    public class GenericRepository<TEntity, TKey>
-        where TEntity : class, IEntity, new()
-        where TKey : struct
+    public class GenericRepository<T>
+        where T : class, IEntity, new()
     {
-        public TEntity CreateNewItem()
+        private readonly List<T> _items = new();
+
+        public T GetByID(int id)
         {
-            return new TEntity();
+            return _items.Single(item => item.Id == id);
         }
 
-        public TKey? Key { get; set; }
-
-        protected readonly List<TEntity> _items = new();
-
-        public void Add(TEntity item)
+        public void Add(T item)
         {
             item.Id = _items.Count + 1;
             _items.Add(item);
         }
 
-        public TEntity GetByID(int id)
+        public void Remove(T item)
         {
-            return default(TEntity);
-          //  return _items.Single(item => item.Id == id);
+            _items.Remove(item);
         }
 
         public void Save()
@@ -33,6 +29,11 @@ namespace MotoApp.Repositores
             {
                 Console.WriteLine(item);
             }
+        }
+
+        public T CreateNewItem()
+        {
+            return new T();
         }
     }
 }
