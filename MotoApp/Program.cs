f68e2657cb64a370cff86764afae42cf1c33dd6e
PawelRa <pawelrat@poczta.onet.pl>
2023-11-15 18:05:17 +0100

    Implementacja SQL Repository

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,7 +1,18 @@
 ﻿using MotoApp.Repositores;
 using MotoApp.Entities;
+using MotoApp.Data;
 
-var employeeGenericRepository = new GenericRepository<Employee>();
-employeeGenericRepository.Add(new Employee { FirstName = "Adam" });
-employeeGenericRepository.Add(new Employee { FirstName = "Zuzia" });
-employeeGenericRepository.Save();
+//var employeeGenericRepository = new GenericRepository<Employee>();
+//employeeGenericRepository.Add(new Employee { FirstName = "Adam" });
+//employeeGenericRepository.Add(new Employee { FirstName = "Zuzia" });
+//employeeGenericRepository.Add(new Employee { FirstName = "Piotr" });
+//employeeGenericRepository.Save();
+
+var SqlRepository = new SqlRepository(new MotoAppDbContext());
+SqlRepository.Add(new Employee { FirstName = "Adam" });
+SqlRepository.Add(new Employee { FirstName = "Zuzia" });
+SqlRepository.Add(new Employee { FirstName = "Piotr" });
+SqlRepository.Save();
+
+var emp = SqlRepository.GetById(1);
+Console.WriteLine(emp.ToString());
